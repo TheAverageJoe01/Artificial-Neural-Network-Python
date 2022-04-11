bc4374b561147ed069520622b02f0689ffa8debd
Joe Smith <joseph.c.smith2014@gmail.com>
2022-04-11 16:28:56 +0100

    Update AI 1.py

--- AI 1.py
+++ AI 1.py
@@ -3,11 +3,12 @@ import random
  
 class node:
     
-    def __init__(self,_nodeNum, _weights, _inputs, _outputs):
+    def __init__(self,_nodeNum, _weights, _inputs, _outputs,_expectedOutput):
         self.inputs = _inputs
         self.outputs = _outputs 
         self.weights = _weights
         self.nodeNum = _nodeNum
+        self.expectedOutput = _expectedOutput
 
         
     def net(self):
@@ -23,8 +24,7 @@ class node:
    
    
 class Network():
-    def __init__(self,_inputs,_hidden,_output):
-        self.inputs = _inputs
+    def __init__(self,_hidden,_output):
         self.hidden = _hidden
         self.output = _output
     
@@ -32,43 +32,33 @@ class Network():
         fwoutput = []
         for node in self.hidden:
             fwoutput.append(node.sigmoid())
-            node.output = node.sigmoid()
+            node.outputs = node.sigmoid()
         fwoutput = [1] + fwoutput
         for node in self.output:
             node.inputs = fwoutput
         fwoutput = []
         for node in self.output:
             fwoutput.append(node.net())
-            node.output = node.net()
+            node.outputs = node.net()
             #print(f"{node.nodeNum} --> {node.net()}")
         #for node in self.hidden + self.output:
             #print(node.nodeNum,node.output)
-        
-    def errorcalc(self):
-        target = [1,0]
-        for node in self.output:
-            for i in range(0(node.output)):
-                n7_error = target[0] - node.output[0]
-            print (n7_error)
 
     
-      
-    def backwardstep(self):
-        pass
         
         
              
 
 nodelist = []
-node4 = node(4,[0.9,0.74,0.8,0.35],[],[])      
+node4 = node(4,[0.9,0.74,0.8,0.35],[],[],[])      
 nodelist.append(node4)       
-node5 = node(5,[0.45,0.13,0.4,0.97],[],[])
+node5 = node(5,[0.45,0.13,0.4,0.97],[],[],[])
 nodelist.append(node5)
-node6 = node(6,[0.36,0.68,0.1,0.96],[],[])
+node6 = node(6,[0.36,0.68,0.1,0.96],[],[],[])
 nodelist.append(node6)
-node7 = node(7,[0.98,0.35,0.5,0.9],[],[])
+node7 = node(7,[0.98,0.35,0.5,0.9],[],[],[1,1,1,0,0,0])
 nodelist.append(node7)
-node8 = node(8,[0.92,0.8,0.13,0.8],[],[])
+node8 = node(8,[0.92,0.8,0.13,0.8],[],[],[0,0,0,1,1,1])
 nodelist.append(node8)
     
 
@@ -88,4 +78,3 @@ outputLayer = [node7,node8]
     
 n = Network(data[0],hiddenLayer,outputLayer)
 n.forwardstep()
-n.errorcalc()
