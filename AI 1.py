a8f7eb8b72e6457aa8adab943820f88b88079562
Joe Smith <joseph.c.smith2014@gmail.com>
2022-04-18 17:08:56 +0100

    Update AI 1.py

--- AI 1.py
+++ AI 1.py
@@ -1,6 +1,7 @@
 import math
 import random
- 
+import matplotlib.pyplot as plt
+
 class node:
     
     def __init__(self,_nodeNum, _weights, _inputs, _outputs,_expectedOutput,_errorRate):
@@ -53,7 +54,7 @@ class Network():
         for node in self.hidden:
             W = []
             for outputnode in self.output:
-                W.append(outputnode.weights[self.hidden.index(node)])
+                W.append(outputnode.weights[self.hidden.index(node) + 1])
             node.errorRate = (node.outputs * (1 - node.outputs) * ((error[0] * W[0] + error[1] * W[1])))
 
     def weightUpdate(self):
@@ -64,16 +65,30 @@ class Network():
             for i in range(len(node.inputs)):
                node.weights[i] += 0.1 * node.errorRate * node.inputs[i] 
         for node in self.hidden + self.output:
-            print(node.nodeNum,"--->",node.weights)
+            print(node.nodeNum,"--->",node.weights,"\n")
 
     def squared_error(self):
+        sumn = 0
         for node in self.output:
-            squared_error = (node.errorRate**2) 
+            sumn += (node.errorRate ** 2)
 
-    def graph(self):
-        pass
+        return sumn * 0.5
+        
+def graph(Epoch , SQ_error):
+    x = Epoch
+    y = SQ_error
+    
+    plt.plot(x,y)
+    # naming the x axis
+    plt.xlabel('x - Epoch')
+    # naming the y axis
+    plt.ylabel('y - Squared error')
+    # giving a title to my graph
+    plt.title('squared error graph')
+    plt.show()
+        
 
-
+#list of nodes 
 nodelist = []
 node4 = node(4,[0.9,0.74,0.8,0.35],[],[],[],0)      
 nodelist.append(node4)       
@@ -94,18 +109,35 @@ data = [[1,0.50,1.00,0.75],
 [1,0.50,-0.25,0.13],
 [1,0.01,0.02,0.05]]
 
-
+#seperating the nodes into the specific layers 
 hiddenLayer = [node4,node5,node6]
 outputLayer = [node7,node8]
 
+#MAIN CODE
 n = Network(hiddenLayer,outputLayer)
-for i in range(2):
+Epoch = []
+
+RealError = []
+for epoch in range(500):
+    SQ_error = []
+    Epoch.append(epoch+1)
+    print("\n","epoch ------> ", epoch + 1 )
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     for j in data:
         index = data.index(j)
         for node in nodelist:
             node.inputs.clear()
             for i in j:
                 node.inputs.append(i)
-            n.forwardstep()
-            n.error(index)
-            n.weightUpdate()
+        n.forwardstep()
+        n.error(index)
+        n.weightUpdate()
+        SQ_error.append(n.squared_error())
+    avg_error = 0
+    for error in SQ_error:
+        avg_error += error
+    avg_error = avg_error / 6 
+    RealError.append(avg_error)
+
+
+graph(Epoch, RealError)
